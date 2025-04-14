f104465d3ec410332fb2ce30766895df94852c9e
Myles Henderson <myles@minware.com>
2025-04-14 12:03:10 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 import os
 
-UTILS_VERSION = "81f3a1c44838af09d0f426a1c2f59852b6f1014a"
+UTILS_VERSION = "c3ffa1f96a19cdc383bdb6adf0c578db958962f0"
 
 setup(name='tap-gitlab',
       version='0.9.15',
