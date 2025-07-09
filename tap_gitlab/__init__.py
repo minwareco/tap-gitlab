bf2adbb14c11fa188bb0006b8677dc63a4c73826
Myles Henderson <myles@minware.com>
2025-07-09 12:15:07 -0400

    fix get_selcted_streams

--- tap_gitlab/__init__.py
+++ tap_gitlab/__init__.py
@@ -1290,11 +1290,20 @@ def sync_project(pid, gitLocal, selected_stream_ids):
 
 def get_selected_streams(catalog):
     '''
-    Gets selected streams from a singer.catalog.Catalog object
+    Gets selected streams.  Checks schema's 'selected'
+    first -- and then checks metadata, looking for an empty
+    breadcrumb and mdata with a 'selected' entry
     '''
     selected_streams = []
-    for stream in catalog.get_selected_streams(STATE):
-        selected_streams.append(stream.tap_stream_id)
+    for stream in catalog['streams']:
+        stream_metadata = stream['metadata']
+        if stream['schema'].get('selected', False):
+            selected_streams.append(stream['tap_stream_id'])
+        else:
+            for entry in stream_metadata:
+                # stream metadata will have empty breadcrumb
+                if not entry['breadcrumb'] and entry['metadata'].get('selected',None):
+                    selected_streams.append(stream['tap_stream_id'])
     return selected_streams
 
 def do_sync():
