c337fc11b2f4b7e3db1a8d736e6893f78bafa0cf
Myles Henderson <myles@minware.com>
2025-07-22 19:01:21 -0400

    first pass at batch fetching missing refs

--- tap_gitlab/__init__.py
+++ tap_gitlab/__init__.py
@@ -551,6 +551,74 @@ async def getChangedfilesForCommits(commits, repo_path, hasLocal, gitLocal):
     results = await asyncio.gather(*coros)
     return results
 
+def process_head_commits(gitLocal, repo_path, headRef, headSha, fetchedCommits, commitQ, LOG_PAGE_SIZE):
+    """
+    Process all commits for a given head, handling pagination and parent tracking.
+    Returns the newlyFetchedCommits dictionary.
+    """
+    missingParents = {}
+    offset = 0
+    newlyFetchedCommits = {}
+    
+    while True:
+        # Get commits one page at a time
+        commits = gitLocal.getCommitsFromHeadPyGit(repo_path, headSha,
+            limit = LOG_PAGE_SIZE, offset = offset, skipAtCommits=fetchedCommits)
+        
+        for commit in commits:
+            # Skip commits we've already imported
+            if commit['sha'] in fetchedCommits or commit['sha'] in newlyFetchedCommits:
+                continue
+
+            commitQ.append(commit)
+
+            # Record that we have now fetched this commit
+            newlyFetchedCommits[commit['sha']] = 1
+            # No longer a missing parent
+            missingParents.pop(commit['sha'], None)
+
+            # Keep track of new missing parents
+            for parent in commit['parents']:
+                if not parent['sha'] in fetchedCommits and not parent['sha'] in newlyFetchedCommits:
+                    missingParents[parent['sha']] = 1
+
+        # If there are no missing parents, then we are done prior to reaching the last page
+        if not missingParents:
+            break
+        elif len(commits) > 0:
+            offset += LOG_PAGE_SIZE
+        # Else if we have reached the end of our data but not found the parents, then we
+        # have a problem
+        else:
+            raise Exception('Some commit parents never found for {}: {}'.format(
+                headRef, ','.join(missingParents.keys())))
+    
+    return newlyFetchedCommits
+
+def fetch_missing_refs_batch(gitLocal, repo_path, missing_refs):
+    """
+    Attempt to fetch all missing refs in a single batch operation.
+    Returns list of refs that should be processed (empty list if fetch failed).
+    """
+    if not missing_refs:
+        return []
+    
+    # Extract just the SHAs for the batch fetch
+    missing_shas = [ref_info['sha'] for ref_info in missing_refs]
+    
+    LOGGER.info('Attempting to batch fetch {} missing refs for {}'.format(len(missing_shas), repo_path))
+    
+    try:
+        # Call the GitLocal batch fetch method from minware-singer-utils PR #12
+        gitLocal.fetchMultipleCommits(repo_path, missing_shas, 'gitlab')
+        LOGGER.info('Successfully batch fetched {} refs for {}'.format(len(missing_shas), repo_path))
+        return missing_refs  # Return all refs for processing
+            
+    except Exception as e:
+        LOGGER.warning('Batch fetch failed with exception for {}: {}'.format(repo_path, str(e)))
+        return []
+
+
 def sync_commit_files(project, heads, gitLocal, commits_only=False, selected_stream_ids=None):
     entity = 'commit_files_meta' if commits_only else 'commit_files'
     stream = CATALOG.get_stream(entity)
@@ -587,6 +655,7 @@ def sync_commit_files(project, heads, gitLocal, commits_only=False, selected_str
 
     # First, walk through all the heads and queue up all the commits that need to be imported
     commitQ = []
+    missing_refs = []  # Track missing refs for batch fetch
 
     for headRef in heads:
         count += 1
@@ -615,53 +684,43 @@ def sync_commit_files(project, heads, gitLocal, commits_only=False, selected_str
                 rec = transformer.transform(refRecord, RESOURCES[refentity]["schema"], refmdata)
             singer.write_record('refs', rec, time_extracted=extraction_time)
 
-        # Maintain a list of parents we are waiting to see
-        missingParents = {}
-
         # Verify that this commit exists in our mirrored repo
         hasLocal = gitLocal.hasLocalCommit(repo_path, headSha, 'gitlab')
         if not hasLocal:
-            LOGGER.warning('MISSING REF/COMMIT {}/{}/{}'.format(repo_path, headRef, headSha))
+            LOGGER.debug('MISSING REF/COMMIT {}/{}/{} - will attempt batch fetch'.format(repo_path, headRef, headSha))
+            missing_refs.append({'ref': headRef, 'sha': headSha})
             continue
 
-        offset = 0
-        newlyFetchedCommits = {}
-        while True:
-            # Get commits one page at a time
-            if hasLocal:
-                commits = gitLocal.getCommitsFromHeadPyGit(repo_path, headSha,
-                    limit = LOG_PAGE_SIZE, offset = offset, skipAtCommits=fetchedCommits)
-            extraction_time = singer.utils.now()
-            for commit in commits:
-                # Skip commits we've already imported
-                if commit['sha'] in fetchedCommits or commit['sha'] in newlyFetchedCommits:
-                    continue
-
-                commitQ.append(commit)
-
-                # Record that we have now fetched this commit
-                newlyFetchedCommits[commit['sha']] = 1
-                # No longer a missing parent
-                missingParents.pop(commit['sha'], None)
-
-                # Keep track of new missing parents
-                for parent in commit['parents']:
-                    if not parent['sha'] in fetchedCommits and not parent['sha'] in newlyFetchedCommits:
-                        missingParents[parent['sha']] = 1
-
-            # If there are no missing parents, then we are done prior to reaching the lst page
-            if not missingParents:
-                break
-            elif hasLocal and len(commits) > 0:
-                offset += LOG_PAGE_SIZE
-            # Else if we have reached the end of our data but not found the parents, then we
-            # have a problem
-            else:
-                raise Exception('Some commit parents never found: ' + \
-                    ','.join(missingParents.keys()))
-
+        # Process all commits for this head
+        extraction_time = singer.utils.now()
+        newlyFetchedCommits = process_head_commits(gitLocal, repo_path, headRef, headSha, 
+                                                  fetchedCommits, commitQ, LOG_PAGE_SIZE)
+        
         # After successfully processing all commits for this head, add them to fetchedCommits
         fetchedCommits.update(newlyFetchedCommits)
+
+    # Attempt to batch fetch any missing refs before processing commits
+    if missing_refs:
+        LOGGER.info('Found {} missing refs, attempting batch fetch for {}'.format(len(missing_refs), repo_path))
+        fetched_refs = fetch_missing_refs_batch(gitLocal, repo_path, missing_refs)
+        
+        if fetched_refs:
+            LOGGER.info('Batch fetch succeeded, processing {} refs'.format(len(fetched_refs)))
+            
+            # Process the fetched refs
+            for ref_info in fetched_refs:
+                headRef = ref_info['ref']
+                headSha = ref_info['sha']
+                
+                LOGGER.debug('Processing batch-fetched ref {}/{}'.format(headRef, headSha))
+                
+                # Process this head's commits using the extracted function
+                newlyFetchedCommits = process_head_commits(gitLocal, repo_path, headRef, headSha,
+                                                          fetchedCommits, commitQ, LOG_PAGE_SIZE)
+                
+                fetchedCommits.update(newlyFetchedCommits)
+        else:
+            LOGGER.warning('Batch fetch failed for {} refs in {}'.format(len(missing_refs), repo_path))
 
     # Now run through all the commits in parallel
     gc.collect()
